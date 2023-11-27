68dd94664d28dac2145ff805923d0463eb27bd7f
Matteo Collina <hello@matteocollina.com>
2023-11-27 23:38:05 +0100

    do not double compile on watch
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/basic.test.js
+++ test/basic.test.js
@@ -4,7 +4,7 @@ import runWithTypeScript from '../lib/run.js'
 import { join } from 'desm'
 
 test('ts-esm', async (t) => {
-  const { strictEqual, completed } = tspl(t, { plan: 2 })
+  const { strictEqual, completed, match } = tspl(t, { plan: 4 })
   const config = {
     files: [],
     cwd: join(import.meta.url, '..', 'fixtures', 'ts-esm')
@@ -13,6 +13,11 @@ test('ts-esm', async (t) => {
   const stream = await runWithTypeScript(config)
 
   const names = new Set(['add', 'add2'])
+
+  stream.once('data', (test) => {
+    strictEqual(test.type, 'test:diagnostic')
+    match(test.data.message, /TypeScript compilation complete \(\d+ms\)/)
+  })
 
   stream.on('test:pass', (test) => {
     strictEqual(names.has(test.name), true)
