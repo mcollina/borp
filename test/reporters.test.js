19c5edc949ab9a4f6419461832acc2b8c3a7d2fd
Matteo Collina <hello@matteocollina.com>
2024-01-30 19:36:19 +0100

    fixup
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/reporters.test.js
+++ test/reporters.test.js
@@ -2,9 +2,11 @@ import { MarkdownReporter, GithubWorkflowFailuresReporter } from '../lib/reporte
 import { test, describe } from 'node:test'
 import { strictEqual } from 'node:assert'
 
+const cwd = process.platform === 'win32' ? 'C:\\foo' : '/foo'
+
 describe('MarkdownReporter', async () => {
   test('should write a report', async () => {
-    const reporter = new MarkdownReporter({ cwd: '/foo' })
+    const reporter = new MarkdownReporter({ cwd })
 
     // This is skipped
     reporter.write({
@@ -65,7 +67,7 @@ describe('MarkdownReporter', async () => {
   })
 
   test('skip fail heading if no failing tests', async () => {
-    const reporter = new MarkdownReporter({ cwd: '/foo' })
+    const reporter = new MarkdownReporter({ cwd })
 
     reporter.write({
       type: 'test:pass',
@@ -109,7 +111,7 @@ describe('MarkdownReporter', async () => {
 
 describe('GithubWorkflowFailuresReporter', async () => {
   test('should write error in github format', async () => {
-    const reporter = new GithubWorkflowFailuresReporter({ cwd: '/foo' })
+    const reporter = new GithubWorkflowFailuresReporter({ cwd })
 
     // This is skipped
     reporter.write({
