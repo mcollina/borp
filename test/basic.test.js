15061f7bf7b7a3ed9f56a2c9ae3894367ff98816
iamdoron <iamdoron@users.noreply.github.com>
2024-03-07 15:25:49 +0200

    Add a monorepo (project references) support for via the --build flag (#19)
    * Add monorepo support via the --build flag
    
    * use --build also on watch
    
    * change outDir to be relative to package1
    
    * temporarily execlude windows & nodejs v21 from ci due to an EPREM bug on node v21

--- test/basic.test.js
+++ test/basic.test.js
@@ -2,6 +2,7 @@ import { test } from 'node:test'
 import { tspl } from '@matteo.collina/tspl'
 import runWithTypeScript from '../lib/run.js'
 import { join } from 'desm'
+import { execa } from 'execa'
 
 test('ts-esm', async (t) => {
   const { strictEqual, completed, match } = tspl(t, { plan: 4 })
@@ -141,7 +142,31 @@ test('src-to-dist', async (t) => {
 
   await completed
 })
+test('monorepo', async (t) => {
+  const { strictEqual, completed, match, deepEqual } = tspl(t, { plan: 5 })
+  const config = {
+    files: [],
+    cwd: join(import.meta.url, '..', 'fixtures', 'monorepo/package2')
+  }
 
+  await execa('npm', ['install'], { cwd: join(import.meta.url, '..', 'fixtures', 'monorepo') })
+  const stream = await runWithTypeScript(config)
+
+  const names = new Set(['package2-add', 'package2-add2'])
+
+  stream.once('data', (test) => {
+    strictEqual(test.type, 'test:diagnostic')
+    match(test.data.message, /TypeScript compilation complete \(\d+ms\)/)
+    deepEqual(test.data.typescriptCliArgs, ['--build'])
+  })
+
+  stream.on('test:pass', (test) => {
+    strictEqual(names.has(test.name), true)
+    names.delete(test.name)
+  })
+
+  await completed
+})
 test('only-src', async (t) => {
   const { strictEqual, completed, match } = tspl(t, { plan: 4 })
   const config = {
