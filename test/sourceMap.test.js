1397adaa50274899a3e3d5bee2d7f98bc68e9f60
Michele Palamidessi <michele.palamidessi@protonmail.com>
2024-05-20 12:58:46 +0200

    feat: enable source map when watch is active

--- test/sourceMap.test.js
+++ test/sourceMap.test.js
@@ -2,9 +2,14 @@
 
 import { test } from 'node:test'
 import { execa } from 'execa'
-import { join } from 'desm'
 import { existsSync } from 'node:fs'
 import { AssertionError, equal, fail, match, strictEqual } from 'node:assert'
+import { tspl } from '@matteo.collina/tspl'
+import path from 'node:path'
+import { cp, mkdtemp, rm, writeFile } from 'node:fs/promises'
+import runWithTypeScript from '../lib/run.js'
+import { once } from 'node:events'
+import { join } from 'desm'
 
 test('borp should return ts file path in error message when sourceMap is active in tsconfig', async (t) => {
   const borp = join(import.meta.url, '..', 'borp.js')
@@ -27,4 +32,69 @@ test('borp should return ts file path in error message when sourceMap is active 
 
   const fileMapPath = join(import.meta.url, '..', 'fixtures', 'ts-esm-source-map', 'dist', 'src', 'add.js.map')
   strictEqual(existsSync(fileMapPath), true, 'Map file should be generated')
+})
+
+test('source map should be generated when watch is active', async (t) => {
+  const { strictEqual, completed, match } = tspl(t, { plan: 4 })
+
+  const dir = path.resolve(await mkdtemp('.test-watch'))
+  await cp(join(import.meta.url, '..', 'fixtures', 'ts-esm-source-map'), dir, {
+    recursive: true
+  })
+
+  const passTest = `
+import { test } from 'node:test'
+import { add } from '../src/add.js'
+import { strictEqual } from 'node:assert'
+
+test('addSuccess', () => {
+  strictEqual(add(1,2), 3)
+})
+`
+  await writeFile(path.join(dir, 'test', 'add.test.ts'), passTest)
+
+  const controller = new AbortController()
+  t.after(async () => {
+    controller.abort()
+    try {
+      await rm(dir, { recursive: true, retryDelay: 100, maxRetries: 10 })
+    } catch {}
+  })
+
+  const config = {
+    files: [],
+    cwd: dir,
+    signal: controller.signal,
+    watch: true
+  }
+
+  const stream = await runWithTypeScript(config)
+
+  const fn = (test) => {
+    if (test.type === 'test:fail') {
+      strictEqual(test.data.name, 'addFailure')
+      stream.removeListener('data', fn)
+    }
+  }
+  stream.on('data', fn)
+
+  const [test] = await once(stream, 'data')
+  strictEqual(test.type, 'test:diagnostic')
+  match(test.data.message, /TypeScript compilation complete \(\d+ms\)/)
+
+  const failureTestToWrite = `
+import { test } from 'node:test'
+import { add } from '../src/add.js'
+import { strictEqual } from 'node:assert'
+
+test('addFailure', () => {
+  strictEqual(add(1,2), 4)
+})
+`
+  await writeFile(path.join(dir, 'test', 'add.test.ts'), failureTestToWrite)
+
+  const expectedMapFilePath = path.join(dir, 'dist', 'src', 'add.js.map')
+  strictEqual(existsSync(expectedMapFilePath), true, 'Map file should be generated')
+
+  await completed
 })
